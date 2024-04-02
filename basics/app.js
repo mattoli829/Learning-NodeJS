5a0b5870587ddc0e46d3001ba37d13abb51ac20e
Matthew Oliveira <83239400+mattoli829@users.noreply.github.com>
2024-04-01 20:17:45 -0400

    First nodeJS Project

---
+++ basics/app.js
@@ -0,0 +1,25 @@
+const readline = require('readline');  // import readline module to interact with the console
+
+const rl = readline.createInterface({ // Create an interface for reading input from stdin and writing to stdout
+    input: process.stdin,  // read data entered by the user in the console. read input provided by user.
+    output: process.stdout // write output to console
+}); // note to self: end with semicolon!
+
+rl.question("Please enter your name: ", (name)=> { // Ask for name and handle input
+    console.log("You entered: "+name); // Log name
+    rl.close(); // close readline interface after getting the input
+})
+
+
+rl.on('close', () => {  // event listener for when the interface closes 
+    console.log("Interface closed"); // log message when interface closes 
+    process.exit(0); // exit
+})
+
+
+/// side notes:
+
+//const square = x => x * x;
+//console.log(square(5)); //output 25
+// arg => expr
+// arrow function that takes argument x and returns it value squared.
